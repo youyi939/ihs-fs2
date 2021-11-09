f5eea55909b2f184f67abb35e66c3a1b5d4f21ed
youyi <46435808+youyi939@users.noreply.github.com>
2021-11-09 14:08:22 +0800

    修改机组数据实时查询接口 格式化数据，小数点后一至两位

--- src/main/java/com/hsgrjt/fushun/ihs/system/entity/HeatNetworkData.java
+++ src/main/java/com/hsgrjt/fushun/ihs/system/entity/HeatNetworkData.java
@@ -25,106 +25,157 @@ public class HeatNetworkData {
     @ApiModelProperty("换热站id")
     private Integer stationId;
 
+    @ApiModelProperty("联网面积")
     private double totalArea;
 
+    @ApiModelProperty("实供面积")
     private double supplyArea;
 
+    @ApiModelProperty("一网供温")
     private double firstSuTempature;
 
+    @ApiModelProperty("一网回温")
     private double firstReTempature;
 
+    @ApiModelProperty("一网供压")
     private double firstSuPressure;
 
+    @ApiModelProperty("一网回压")
     private double firstRePressure;
 
+    @ApiModelProperty("二网供温")
     private double secSuTempature;
 
+    @ApiModelProperty("二网回温")
     private double secReTempature;
 
+    @ApiModelProperty("二网供压")
     private double secSuPressure;
 
+    @ApiModelProperty("二网回压")
     private double secRePressure;
 
+    @ApiModelProperty("温度目标设定")
     private double targetTempature;
 
+    @ApiModelProperty("二网压力上限")
     private double secPressureMax;
 
+    @ApiModelProperty("二网压力设定上限")
     private double secPressureSetMax;
 
+    @ApiModelProperty("二网压力设定下限")
     private double secPressureSetMin;
 
+    @ApiModelProperty("二网压力下限")
     private double secPressureMin;
 
+    @ApiModelProperty("一阀给定")
     private double mainValveSet;
 
+    @ApiModelProperty("一阀反馈")
     private double mainValveFeed;
 
+    @ApiModelProperty("二阀给定")
     private double auxValveSet;
 
+    @ApiModelProperty("二阀反馈")
     private double auxValveFeed;
 
+    @ApiModelProperty("水箱水位")
     private double waterTankLevel;
 
+    @ApiModelProperty("水箱水位下限")
     private double waterTankMinLevel;
 
+    @ApiModelProperty("循环泵启动")
     private Integer circulatingPumpOn;
 
+    @ApiModelProperty("循环泵给定")
     private double circulatingPumpSet;
 
+    @ApiModelProperty("循环泵频率")
     private double circulatingPumpFeed;
 
+    @ApiModelProperty("循环泵故障代码")
     private Integer circulatingPumpCode;
 
+    @ApiModelProperty("补水泵启动")
     private Integer replenishingPumpOn;
 
+    @ApiModelProperty("补水泵给定")
     private double replenishingPumpSet;
 
+    @ApiModelProperty("补水泵频率")
     private double replenishingPumpFeed;
 
+    @ApiModelProperty("补水泵故障代码")
     private Integer replenishingPumpCode;
 
+    @ApiModelProperty("一网流量设定")
     private double firstFlowSet;
 
+    @ApiModelProperty("一网流量单耗")
     private double firstConsumption;
 
+    @ApiModelProperty("一网流量")
     private double firstFlow;
 
+    @ApiModelProperty("一网流量累计")
     private double firstFlowSum;
 
+    @ApiModelProperty("一网热量")
     private double firstInstantHeat;
 
+    @ApiModelProperty("一网热量累计")
     private double firstHeatTotal;
 
+    @ApiModelProperty("预测热量")
     private double firstHeatPredict;
 
+    @ApiModelProperty("补水流量")
     private double replenishingFlow;
 
+    @ApiModelProperty("补水流量累计")
     private double replenishingTotal;
 
+    @ApiModelProperty("二网流量")
     private double secFlow;
 
+    @ApiModelProperty("二网流量累计")
     private double secFlowSum;
 
+    @ApiModelProperty("瞬时电量")
     private double electricPower;
 
+    @ApiModelProperty("电量累计")
     private double electricTotal;
 
+    @ApiModelProperty("热负荷")
     private double heatConsumption;
 
+    @ApiModelProperty("一网流量、年初始")
     private double firstFlowStart;
 
+    @ApiModelProperty("一网热量、年初始")
     private double firstHeatStart;
 
+    @ApiModelProperty("补水流量、年初始")
     private double replenishingStart;
 
+    @ApiModelProperty("电量、年初始")
     private double electricStart;
 
+    @ApiModelProperty("平均室温")
     private double roomTempatureAvg;
 
+    @ApiModelProperty("地面积水报警")
     private Integer leaking;
 
+    @ApiModelProperty("调试状态开启")
     private Integer debug;
 
+    @ApiModelProperty("数据时间")
     private Date gmtCreate;
 
 
--- src/main/java/com/hsgrjt/fushun/ihs/system/service/impl/HeatNetworkDataServiceImpl.java
+++ src/main/java/com/hsgrjt/fushun/ihs/system/service/impl/HeatNetworkDataServiceImpl.java
@@ -16,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.xml.crypto.Data;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -110,11 +112,86 @@ public class HeatNetworkDataServiceImpl implements HeatNetworkDataService {
             Date dNow = data.getGmtCreate();
             SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
             dto.setCreateDate(ft.format(dNow));
+            dto = initData(dto);
+
             heatNetworkDataList.add(dto);
         }
 
 
         return R.ok("查询成功").putData(heatNetworkDataList);
+    }
+
+
+    /**
+     * 格式化对象中的小数，在查询前调用
+     * @param dto 格式化前的对象
+     * @return 格式化后的对象
+     */
+    public static HeatNetworkDataDTO initData(HeatNetworkDataDTO dto){
+        //一级网参数
+        dto.setFirstSuTempature(parseData(dto.getFirstSuTempature(),1)); //一网供温
+        dto.setFirstReTempature(parseData(dto.getFirstReTempature(),1));  //一网回温
+        dto.setFirstFlow(parseData(dto.getFirstFlow(),1));              //一网流量
+        dto.setFirstSuPressure(parseData(dto.getFirstSuPressure(),2));  //一网供压
+        dto.setFirstRePressure(parseData(dto.getFirstRePressure(),2));  //一网回压
+        dto.setFirstFlowSet(parseData(dto.getFirstFlowSet(),1));  //一网流量设定
+        dto.setFirstConsumption(parseData(dto.getFirstConsumption(),1));  //一网流量单耗
+        dto.setFirstFlowSum(parseData(dto.getFirstFlowSum(),1));  //一网流量累计
+        dto.setFirstInstantHeat(parseData(dto.getFirstInstantHeat(),1));  //一网热量
+//        dto.setFirstHeatTotal(parseData(dto.getFirstHeatTotal(),1));  //一网热量累计
+
+        //二级网参数
+        dto.setSecSuTempature(parseData(dto.getSecSuTempature(),1)); //二网供温
+        dto.setSecReTempature(parseData(dto.getSecReTempature(),1)); //二网回温
+        dto.setSecFlow(parseData(dto.getSecFlow(),1));               //二网流量
+        dto.setSecSuPressure(parseData(dto.getSecSuPressure(),2));  //二网供压
+        dto.setSecRePressure(parseData(dto.getSecRePressure(),2));  //二网回压
+        dto.setSecFlowSum(parseData(dto.getSecFlowSum(),1));  //二网流量累计
+
+        //循环泵
+        dto.setCirculatingPumpSet(parseData(dto.getCirculatingPumpSet(),1));  //循环泵给定
+        dto.setCirculatingPumpFeed(parseData(dto.getCirculatingPumpFeed(),1));  //循环泵频率
+
+        dto.setTargetTempature(parseData(dto.getTargetTempature(),1));  //温度目标设定
+        dto.setSecPressureMax(parseData(dto.getSecPressureMax(),1));  //二网压力上限
+        dto.setSecPressureSetMax(parseData(dto.getSecPressureSetMax(),1));  //二网压力设定上限
+        dto.setSecPressureSetMin(parseData(dto.getSecPressureSetMin(),1));  //二网压力设定下限
+        dto.setSecPressureMin(parseData(dto.getSecPressureMin(),1));  //二网压力下限
+        dto.setMainValveSet(parseData(dto.getMainValveSet(),1));  //一阀给定
+        dto.setMainValveFeed(parseData(dto.getMainValveFeed(),1));  //一阀反馈
+        dto.setAuxValveSet(parseData(dto.getAuxValveSet(),1));  //二阀给定
+        dto.setAuxValveFeed(parseData(dto.getAuxValveFeed(),1));  //二阀反馈
+        dto.setWaterTankLevel(parseData(dto.getWaterTankLevel(),1));  //水箱水位
+        dto.setWaterTankMinLevel(parseData(dto.getWaterTankMinLevel(),1));  //水箱水位下限
+        dto.setReplenishingPumpSet(parseData(dto.getReplenishingPumpSet(),1));  //补水泵给定
+        dto.setReplenishingPumpFeed(parseData(dto.getReplenishingPumpFeed(),1));  //补水泵频率
+        dto.setFirstHeatPredict(parseData(dto.getFirstHeatPredict(),1));  //预测热量
+        dto.setReplenishingFlow(parseData(dto.getReplenishingFlow(),1));  //补水流量
+//        dto.setReplenishingTotal(parseData(dto.getReplenishingTotal(),1));  //补水流量累计
+        dto.setElectricPower(parseData(dto.getElectricPower(),1));  //瞬时电量
+        dto.setElectricTotal(parseData(dto.getElectricTotal(),1));  //电量累计
+        dto.setHeatConsumption(parseData(dto.getHeatConsumption(),1));  //热负荷
+        dto.setRoomTempatureAvg(parseData(dto.getRoomTempatureAvg(),2));  //平均室温
+
+
+        return dto;
+    }
+
+    /**
+     * 保留小数点后几位的方法
+     * @param d 小数
+     * @param num 保留到小数点后几位
+     * @return 保留后的小数
+     */
+    private static double parseData(double d, int num){
+
+        NumberFormat nf = NumberFormat.getNumberInstance();
+// 保留两位小数
+        nf.setMaximumFractionDigits(num);
+// 如果不需要四舍五入，可以使用RoundingMode.DOWN
+        nf.setRoundingMode(RoundingMode.UP);
+
+        return Double.parseDouble(nf.format(d));
     }
 
 
