d8a8ce05f3a47b04da0129fac62521e9ab4eebaa
youyi <46435808+youyi939@users.noreply.github.com>
2021-12-02 13:03:42 +0800

    修复定时器代码

--- src/main/java/com/hsgrjt/fushun/ihs/IhsApplication.java
+++ src/main/java/com/hsgrjt/fushun/ihs/IhsApplication.java
@@ -4,7 +4,9 @@ import com.hsgrjt.fushun.ihs.timer.TimedTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class IhsApplication {
 
